fd57ecb25fa9871daa59d3ae88eab41fa5cb2d4d
lukman008 <bellolukman007@gmail.com>
2018-07-11 01:00:49 +0100

    update

--- routes/switxh.js
+++ routes/switxh.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient();
-
+const assert = require('assert');
 const url = 'mongodb://luqman:meleoron43@ds231961.mlab.com:31961/heroku_39xp0czl';
 
 const dbName = 'heroku_39xp0czl';
@@ -9,59 +9,65 @@ const dbName = 'heroku_39xp0czl';
 const connection = MongoClient.connect(url);
 
 
-router.get('/', function(req, res, next) {
-  connection.then(function(db){
+router.get('/', function (req, res, next) {
+  connection.then(function (db) {
     db = db.db(dbName);
-    db.createCollection('Switxh',function(err,collection){
-        assert.strictEqual(null,err);
-        collection.findOne({_id:'dd-swx-001'}, function(err,result) {
-          assert.strictEqual(err,null);
+    db.createCollection('Switxh', function (err, collection) {
+      assert.strictEqual(null, err);
+      collection.findOne({ _id: 'dd-swx-001' }, function (err, result) {
+        assert.strictEqual(err, null);
+        res.json(result);
+      });
+    });
+  });
+});
+
+router.get('/app', function (req, res, next) {
+  var query;
+  try {
+    query = JSON.parse(req.query);
+  } catch (err) {
+    res.end("Invalid parameters");
+    return;
+  }
+
+
+  connection.then(function (db) {
+    db = db.db(dbName);
+    db.createCollection('Switxh', function (err, collection) {
+      assert.strictEqual(null, err);
+      collection.update({ _id: "dd-swx-001" }, { a: query.a, b: query.b, c: query.c, d: query.d, dateModified: Date.now(), caller: "app" }, { upsert: true }, function (err, result) {
+        assert.strictEqual(err, null);
+        collection.findOne({ _id: 'dd-swx-001' }, function (err, result) {
+          assert.strictEqual(err, null);
           res.json(result);
         });
+      });
     });
-  });  
+  });
 });
 
-router.get('/app', function(req, res, next) {
+router.get('/hw', function (req, res, next) {
   var query;
-  try{
+  try {
     query = JSON.parse(req.query);
-  }catch(err){
+  } catch (err) {
     res.end("Invalid parameters");
     return;
   }
-
-  
-  connection.then(function(db){
+  connection.then(function (db) {
     db = db.db(dbName);
-    db.createCollection('Switxh',function(err,collection){
-        assert.strictEqual(null,err);
-        collection.update({_id:"dd-swx-001"},{a:query.a,b:query.b,c:query.c,d:query.d,dateModified:Date.now(),caller:"app"},{upsert:true}, function(err,result) {
-          assert.strictEqual(err,null);
-          res.end("OK");
+    db.createCollection('Switxh', function (err, collection) {
+      assert.strictEqual(null, err);
+      collection.update({ _id: "dd-swx-001" }, { power: query.power, voltage: query.voltage, dateModified: Date.now(), caller: "hw" }, { upsert: true }, function (err, result) {
+        assert.strictEqual(err, null);
+        collection.findOne({ _id: 'dd-swx-001' }, function (err, result) {
+          assert.strictEqual(err, null);
+          res.json(result);
         });
+      });
     });
-  });  
-});
-
-router.get('/dev', function(req, res, next) {
-  var query;
-  try{
-    query = JSON.parse(req.query);
-  }catch(err){
-    res.end("Invalid parameters");
-    return;
-  }
-  connection.then(function(db){
-    db = db.db(dbName);
-    db.createCollection('Switxh',function(err,collection){
-        assert.strictEqual(null,err);
-        collection.update({_id:"dd-swx-001"},{power:query.power,voltage:query.voltage,dateModified:Date.now(),},{upsert:true}, function(err,result) {
-          assert.strictEqual(err,null);
-          res.end("OK");
-        });
-    });
-  });  
+  });
 });
 
 module.exports = router;
