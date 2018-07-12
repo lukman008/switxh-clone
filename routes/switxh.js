dfbc6d4d9210c131c4fcb509f5f8b8d6b5c8ba13
lukman008 <bellolukman007@gmail.com>
2018-07-12 02:14:02 +0100

    update

--- routes/switxh.js
+++ routes/switxh.js
@@ -3,11 +3,45 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient();
 const assert = require('assert');
 const url = 'mongodb://luqman:meleoron43@ds231961.mlab.com:31961/heroku_39xp0czl';
-
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://test.mosquitto.org');
 const dbName = 'heroku_39xp0czl';
 
 const connection = MongoClient.connect(url);
 
+
+client.on('connect', function () {
+  client.subscribe('presence');
+  client.publish('presence', 'Hello mqtt');
+});
+
+
+client.on('message', function (topic, message) {
+  console.log(message.toString());
+  var mssg = message.toString();
+  if (mssg.startsWith('dd-swx-001')) {
+    var arr = mssg.split(',');
+    voltage = arr[0];
+    power = arr[1];
+    connection.then(function (db) {
+      db = db.db(dbName);
+      db.createCollection('Switxh', function (err, collection) {
+        assert.strictEqual(null, err);
+        collection.update({ _id: "dd-swx-001" }, { power: power, voltage: voltage, dateModified: Date.now(), caller: "hw" }, { upsert: true }, function (err, result) {
+          assert.strictEqual(err, null);
+          collection.findOne({ _id: 'dd-swx-001' }, function (err, result) {
+            assert.strictEqual(err, null);
+
+            client.publish('presence', JSON.stringify(result));
+            client.end();
+          });
+        });
+      });
+    });
+  }
+
+
+});
 
 router.get('/', function (req, res, next) {
   connection.then(function (db) {
